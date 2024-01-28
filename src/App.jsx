4d050a966460977359f86b2a4a1d2ec9c8740b05
Dallengaller <dallengaller@gmail.com>
2024-01-28 02:47:55 +0100

    corrigiendo componentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import React, { useState } from 'react';
 import Formulario from './components/Formulario';
 import Listado from './components/Listado';
 import { BaseColaboradores } from './BaseColaboradores';
+import Buscador from './components/Buscador';
 
 function App() {
     const [colaboradoresIngresados, setColaboradoresIngresados] = useState([]);
+    const [search, setSearch] = useState('');
 
     const agregarColaborador = (nuevoColaborador) => {
         setColaboradoresIngresados([...colaboradoresIngresados, nuevoColaborador]);
@@ -13,14 +15,39 @@ function App() {
 
     const todosLosColaboradores = [...BaseColaboradores, ...colaboradoresIngresados];
 
+    // Función para manejar el cambio en el campo de búsqueda
+    const handleSearchChange = (e) => {
+        setSearch(e.target.value);
+    };
+
+    // Filtrar colaboradores basado en la búsqueda (por nombre, correo y telefono)
+    const colaboradoresFiltrados = todosLosColaboradores.filter((colaborador) =>
+        colaborador.nombre.toLowerCase().includes(search.toLowerCase()) ||
+        colaborador.correo.toLowerCase().includes(search.toLowerCase()) ||
+        colaborador.telefono.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
         <div className="container d-flex align-items-center justify-content-center vh-100">
             <div className="row">
-                <div className="col-12 col-md-6">
-                    <Formulario onAgregarColaborador={agregarColaborador} />
+                {/* Título y Buscador */}
+                <div className="col-12 mb-4 text-center">
+                    <h2 className="mb-3">Lista de Colaboradores</h2>
+                    <Buscador search={search} onChange={handleSearchChange} />
                 </div>
-                <div className="col-12 col-md-6 mb-4">
-                    <Listado colaboradores={todosLosColaboradores} />
+
+
+                <div className="row justify-content-between">
+
+                    {/* Listado */}
+                    <div className="col-12 col-md-6">
+                        <Listado colaboradores={colaboradoresFiltrados} />
+                    </div>
+
+                    {/* Formulario */}
+                    <div className="col-12 col-md-6">
+                        <Formulario onAgregarColaborador={agregarColaborador} />
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,5 @@
-// Formulario.jsx
 import React, { useState } from 'react';
+import Alert from './Alert';
 
 const Formulario = ({ onAgregarColaborador }) => {
   const [colaborador, setColaborador] = useState({
@@ -8,6 +8,12 @@ const Formulario = ({ onAgregarColaborador }) => {
     edad: '',
     cargo: '',
     telefono: '',
+  });
+
+  const [showAlert, setShowAlert] = useState({
+    status: false,
+    type: '',
+    message: '',
   });
 
   const capturaInput = (e) => {
@@ -20,6 +26,40 @@ const Formulario = ({ onAgregarColaborador }) => {
 
   const agregarNuevoColaborador = (e) => {
     e.preventDefault();
+
+    // Validaciones
+    if (!colaborador.nombre || !colaborador.correo || !colaborador.edad || !colaborador.cargo || !colaborador.telefono) {
+      setShowAlert({
+        status: true,
+        type: 'danger',
+        message: 'Completa todos los campos!',
+      });
+      return;
+    }
+
+    // Validación de formato de correo electrónico
+    const correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!correoRegex.test(colaborador.correo)) {
+      setShowAlert({
+        status: true,
+        type: 'danger',
+        message: 'Ingrese un correo electrónico válido',
+      });
+      return;
+    }
+
+    // Validación de números en el campo de teléfono
+    const telefonoRegex = /^[0-9]+$/;
+    if (!telefonoRegex.test(colaborador.telefono)) {
+      setShowAlert({
+        status: true,
+        type: 'danger',
+        message: 'Ingrese solo números en el campo de teléfono',
+      });
+      return;
+    }
+
+    // Si todas las validaciones son exitosas, agrega al colaborador y muestra una alerta de éxito
     onAgregarColaborador(colaborador);
     setColaborador({
       nombre: '',
@@ -28,11 +68,27 @@ const Formulario = ({ onAgregarColaborador }) => {
       cargo: '',
       telefono: '',
     });
+
+    setShowAlert({
+      status: true,
+      type: 'success',
+      message: 'Colaborador agregado!',
+    });
+
+    // Después de un tiempo, oculta la alerta de éxito
+    setTimeout(() => {
+      setShowAlert({
+        status: false,
+        type: '',
+        message: '',
+      });
+    }, 3000); // 3000 milisegundos (3 segundos)
   };
 
   return (
     <div>
-        <h2>Agregar Colaborador</h2>
+      <h2>Agregar Colaborador</h2>
+
       <form onSubmit={agregarNuevoColaborador}>
         <div className="mb-3">
           <label htmlFor="nombre" className="form-label">
@@ -99,9 +155,15 @@ const Formulario = ({ onAgregarColaborador }) => {
           />
         </div>
 
-        <button type="submit" className="btn btn-primary">
-          Agregar Colaborador
-        </button>
+        {/* Botón y Alerta en el mismo contenedor */}
+        <div className="d-flex flex-column align-items-start">
+          <button type="submit" className="btn btn-primary">
+            Agregar Colaborador
+          </button>
+
+          {/* Alerta */}
+          {showAlert.status && <Alert color={showAlert.type}>{showAlert.message}</Alert>}
+        </div>
       </form>
     </div>
   );
